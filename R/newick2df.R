4116ea08919c146b8efb602ed7beeadedc178b96
Aaron A. King <kingaa@umich.edu>
2020-06-20 10:10:45 -0400

    small changes to 'newick2df'

--- R/newick2df.R
+++ R/newick2df.R
@@ -1,19 +1,20 @@
-##' Tree in newick to dataframe
+##' Convert a tree in Newick format to data frame
 ##'
-##' Covert a tree in newick to a data frame, containing event times, number of lineages, sample indicator, direct descent indicator, and coalescent indicator.
+##' Convert a genealogical tree in Newick format to a data frame, containing event times, number of lineages, sample indicator, direct-descent indicator, and coalescent indicator.
 ##'
 ##' @include package.R
 ##'
-##' @param tree tree data in newick format.
+##' @param tree tree data in Newick format.
 ##'
-##' @return A data frame could be used as \code{pomp} input.
+##' @return A data frame suitable for use as \code{pomp} input.
 ##' 
 ##' @example examples/newick2df.R
 ##'
 ##' @importFrom ape read.tree
 ##' @importFrom ggplot2 fortify
-##' @importFrom dplyr mutate arrange transmute
+##' @importFrom dplyr mutate arrange select
 ##' @importFrom tidyr replace_na
+##' @importFrom utils globalVariables
 ##'
 ##' @name newick2df
 ##' @rdname newick2df
@@ -26,13 +27,19 @@ newick2df <- function(tree) {
   read.tree(text=gsub("\\w+:0.0,","",tree)) %>%
     fortify(ladderize=TRUE) %>%
     arrange(x) %>%
-    mutate(sample=as.numeric(isTip & x!= 0.0),
-           inter=table(parent)[as.character(node)] %>% replace_na(0),
-           dd=as.numeric(inter==1), 
-           coal=as.numeric(inter==2 & x!=0.0),
-           lineages=sum(x==0.00)+cumsum(coal)-cumsum(sample)) %>%
-    mutate(lineages=lineages-min(lineages)) %>%
-    transmute(time=x,lineages=lineages,dd=dd,coal=coal,sample=sample) -> df
-  
-  df
+    mutate(
+      sample=as.numeric(isTip & x != 0),
+      inter=table(parent)[as.character(node)] %>% replace_na(0),
+      dd=as.numeric(inter==1), 
+      coal=as.numeric(inter==2 & x!=0),
+      lineages=sum(x==0)+cumsum(coal)-cumsum(sample)
+    ) %>%
+    mutate(
+      time=x,
+      lineages=lineages-min(lineages)
+    ) %>%
+    select(time,lineages,dd,coal,sample)
 }
+
+utils::globalVariables(c("coal","dd","inter","isTip","lineages",
+  "node","parent","time"))
