df73395d400d04a64eaee01988811d7e824e3d11
Aaron A. King <kingaa@umich.edu>
2021-11-27 09:58:01 -0500

    remove error in dmeasure

--- src/lbdp_pomp.c
+++ src/lbdp_pomp.c
@@ -1,14 +1,14 @@
 #include <pomp.h>
 #include <R_ext/Rdynload.h>
 
-#define lambda		(__p[__parindex[0]])
-#define mu		(__p[__parindex[1]])
-#define n0		(__p[__parindex[2]])
-#define psi		(__p[__parindex[3]])
-#define lineages	(__covars[__covindex[0]])
-#define code		(__covars[__covindex[1]])
-#define n		(__x[__stateindex[0]])
-#define ll		(__x[__stateindex[1]])
+#define lambda          (__p[__parindex[0]])
+#define mu              (__p[__parindex[1]])
+#define n0              (__p[__parindex[2]])
+#define psi             (__p[__parindex[3]])
+#define lineages        (__covars[__covindex[0]])
+#define code            (__covars[__covindex[1]])
+#define n               (__x[__stateindex[0]])
+#define ll              (__x[__stateindex[1]])
 
 void lbdp_rinit (double *__x, const double *__p, double t, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars)
 {
@@ -29,27 +29,24 @@ void lbdp_gill (double *__x, const double *__p, const int *__stateindex, const i
   } else if (ind == 0) {         // dead sample
     ll += (n >= lineages) ? log(psi) : R_NegInf;
   } else if (ind == -1) {        // live sample
-    ll += (n > 0) ? log(psi*n) : R_NegInf;
-    ll += (n > lineages) ? log(1-lineages/n) : R_NegInf;
+    ll += (n > lineages) ? log(psi*(n-lineages)) : R_NegInf;
   }
 
   // Gillespie steps
-  tstep = exp_rand()/(lambda+mu)/n;
-  while (t + tstep < tmax) {
+  tstep = (n > 0) ? exp_rand()/(lambda+mu)/n : R_PosInf;
+  while (R_FINITE(tstep) && t + tstep < tmax) {
     ll -= psi*n*tstep;
-    if (unif_rand() < lambda/(lambda+mu)) {   // birth
-      n += 1;
+    if (unif_rand() < lambda/(lambda+mu)) {
+      n += 1;			// birth
       ll += (n > lineages) ? log(1-lineages*(lineages-1)/n/(n-1)) : R_NegInf;
-    } else {            // death
-      n -= 1;
+    } else {
+      n -= 1;			// death
     }
     t += tstep;
-    tstep = exp_rand()/(lambda+mu)/n;
+    tstep = (n > 0) ? exp_rand()/(lambda+mu)/n : R_PosInf;
   }
-
   tstep = tmax - t;
   ll -= psi*n*tstep;
-   
 }
 
 void lbdp_euler (double *__x, const double *__p, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t, double dt)
@@ -102,11 +99,7 @@ void lbdp_euler (double *__x, const double *__p, const int *__stateindex, const 
 
 void lbdp_dmeas (double *__lik, const double *__y, const double *__x, const double *__p, int give_log, const int *__obsindex, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t)
 {
-  if (n >= 0 && n >= lineages) {
-    lik = (give_log) ? ll : exp(ll);
-  } else {
-    lik = (give_log) ? R_NegInf : 0;
-  }
+  lik = (give_log) ? ll : exp(ll);
 }
 
 #undef lik
