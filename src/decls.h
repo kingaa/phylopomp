0f0a30305afd45fea4b942103a482462e3fdefd5
Aaron A. King <kingaa@umich.edu>
2021-12-03 16:51:15 -0500

    package passes checks

--- src/decls.h
+++ src/decls.h
@@ -15,14 +15,12 @@ extern "C" {
   SEXP playSIRwS (SEXP Beta, SEXP Gamma, SEXP Psi, SEXP S0, SEXP I0, SEXP R0, SEXP Times, SEXP T0, SEXP Tree, SEXP Ill, SEXP State);
   SEXP get_SIRwS_info (SEXP X, SEXP Prune, SEXP Compact);
 
+  SEXP playSIRS (SEXP Beta, SEXP Gamma, SEXP Psi, SEXP Delta, SEXP S0, SEXP I0, SEXP R0, SEXP Times, SEXP T0, SEXP Tree, SEXP Ill, SEXP State);
+  SEXP get_SIRS_info (SEXP X, SEXP Prune, SEXP Compact);
+
   SEXP playmultiSIRwS (SEXP Beta, SEXP Gamma, SEXP Psi, SEXP Theta, SEXP S0, SEXP I0, SEXP R0, SEXP Times, SEXP T0, SEXP Tree, SEXP Ill, SEXP State);
   SEXP get_multiSIRwS_info (SEXP X, SEXP Prune, SEXP Compact);
   
-  void multisir_rinit (double *__x, const double *__p, double t, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars);
-  void multisir_dmeas (double *__lik, const double *__y, const double *__x, const double *__p, int give_log, const int *__obsindex, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t);
-  void birthrates (int indmax, double size, double disp, double *arr);
-  void multisir_gill (double *__x, const double *__p, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t, double dt);
-
 #ifdef __cplusplus
 }
 #endif
