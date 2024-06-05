28c94fedfb648d71c775c54b3e4744d1a75b3d66
Aaron A. King <kingaa@umich.edu>
2024-06-05 17:52:14 -0400

    simpler decls.h

--- Makefile
+++ Makefile
@@ -9,5 +9,5 @@ POMP_HEADERS = $(shell $(REXE) -e 'cat(system.file("include",package="pomp"))')
 
 src/decls.h: $(CSOURCE)
 	file=`mktemp tmpXXXXXXX.h` && \
-	cproto -I $(R_HOME)/include -I $(POMP_HEADERS) -e $(CSOURCE) > $$file && \
+	cproto -f2 -I $(R_HOME)/include -I $(POMP_HEADERS) -e $(CSOURCE) > $$file && \
 	mv $$file $@
--- src/decls.h
+++ src/decls.h
@@ -1,14 +1,14 @@
 /* src/init.c */
-extern void R_init_phylopomp(DllInfo *info);
+extern void R_init_phylopomp(DllInfo *);
 /* src/lbdp_pomp.c */
-extern void lbdp_rinit(double *__x, const double *__p, double t, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars);
-extern void lbdp_gill(double *__x, const double *__p, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t, double Rf_dt);
-extern void lbdp_dmeas(double *__lik, const double *__y, const double *__x, const double *__p, int give_log, const int *__obsindex, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t);
+extern void lbdp_rinit(double *, const double *, double, const int *, const int *, const int *, const double *);
+extern void lbdp_gill(double *, const double *, const int *, const int *, const int *, const double *, double, double);
+extern void lbdp_dmeas(double *, const double *, const double *, const double *, int, const int *, const int *, const int *, const int *, const double *, double);
 /* src/seirs_pomp.c */
-extern void seirs_rinit(double *__x, const double *__p, double t0, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars);
-extern void seirs_gill(double *__x, const double *__p, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t, double Rf_dt);
-extern void seirs_dmeas(double *__lik, const double *__y, const double *__x, const double *__p, int give_log, const int *__obsindex, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t);
+extern void seirs_rinit(double *, const double *, double, const int *, const int *, const int *, const double *);
+extern void seirs_gill(double *, const double *, const int *, const int *, const int *, const double *, double, double);
+extern void seirs_dmeas(double *, const double *, const double *, const double *, int, const int *, const int *, const int *, const int *, const double *, double);
 /* src/sirs_pomp.c */
-extern void sirs_rinit(double *__x, const double *__p, double t, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars);
-extern void sirs_gill(double *__x, const double *__p, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t, double Rf_dt);
-extern void sirs_dmeas(double *__lik, const double *__y, const double *__x, const double *__p, int give_log, const int *__obsindex, const int *__stateindex, const int *__parindex, const int *__covindex, const double *__covars, double t);
+extern void sirs_rinit(double *, const double *, double, const int *, const int *, const int *, const double *);
+extern void sirs_gill(double *, const double *, const int *, const int *, const int *, const double *, double, double);
+extern void sirs_dmeas(double *, const double *, const double *, const double *, int, const int *, const int *, const int *, const int *, const double *, double);
