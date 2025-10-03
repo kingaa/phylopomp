db5e262ada480be31c981194e9c568f95361c2b7
Aaron A. King <kingaa@umich.edu>
2025-10-03 11:37:45 -0400

    update tests

---
+++ tests/twoundead1.R
@@ -0,0 +1,45 @@
+png(filename="twoundead1-%02d.png",res=100)
+
+options(tidyverse.quiet=TRUE,digits=3)
+library(tidyverse)
+library(pomp)
+library(phylopomp)
+
+simulate(
+  "TwoUndead",
+  t0=0,time=3,
+  iota1=0,iota2=0,
+  Beta11=4,Beta12=1,
+  Beta21=1,Beta22=4,
+  gamma1=1,gamma2=1,
+  c1=0.5,c2=0.5,
+  psi1=1,psi2=1,
+  S1_0=100,I1_0=2,R1_0=0,
+  S2_0=100,I2_0=2,R2_0=0,
+  b1=0.1,b2=0.1,
+  d1=0.1,d2=0.1,
+  omega1=0.5,omega2=0.5
+) |>
+  simulate(time=5,c1=1,c2=1) |>
+  freeze(seed=747318644) -> s
+
+stopifnot(
+  s |>
+    yaml() |>
+    textConnection() |>
+    readLines() |>
+    length()==4561
+)
+
+pal <- c("#00274c","#ffcb05","#aaaaaa","#cccccc","#800000")
+
+s |>
+  plot()
+
+s |>
+  plot(obscure=FALSE,palette=pal)
+
+s |>
+  plot(prune=FALSE,obscure=FALSE,palette=pal)
+
+dev.off()
