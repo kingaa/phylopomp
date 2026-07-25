1a11bcc84f1204cbe764bcb747b0bd248e32cfc1
Aaron A. King <kingaa@umich.edu>
2026-07-25 17:48:55 -0400

    improve tests

--- tests/cblv.R
+++ tests/cblv.R
@@ -15,3 +15,59 @@ y1
 
 x |> getInfo(cblv=TRUE) -> y2
 stopifnot(y1==y2$cblv)
+
+n1 <- parse_newick("(a:4,(:1,(b:2,c:1):1):2):1;(:8,:3):1;")
+x1 <- cblv(n1)
+n2 <- parse_newick("(:8,:3):1;(a:4,(:1,(b:2,c:1):1):2):1;")
+x2 <- cblv(n2)
+n3 <- parse_newick("(:8,:3):1;((:1,(:2,:1):1):2,:4):1;")
+x3 <- cblv(n3)
+n4 <- parse_newick("(:8,:3):1;(((:2,:1):1,:1):2,:4):1;")
+x4 <- cblv(n4)
+stopifnot(
+  x1 == x2,
+  x2 == x3,
+  x3 == x4
+)
+
+n <- parse_newick("((a:4,((b:2,c:1):1)):0.5,d:2.5):0.5;")
+x = cblv(n)
+x
+stopifnot(
+  x[,1] == c(5.0,3.0,1.0,2.5),
+  x[,2] == c(1.0,2.0,0.5,0.0)
+)
+
+n <- parse_newick("((a:4,((b:2,c:1):1)):0.5,d:2.5):0;")
+x = cblv(n)
+x
+stopifnot(
+  x[,1] == c(4.5,3.0,1.0,2.5),
+  x[,2] == c(0.5,1.5,0.0,0.0)
+)
+
+## one ternary node, one zero-length branch
+n1 <- parse_newick("((a:4,((b:2,c:1,e:3):1)):1,d:3):1;")
+x1 <- cblv(n1)
+stopifnot(
+  x1[,1] == c(6.0,2.0,1.0,4.0,3.0),
+  x1[,2] == c(3.0,3.0,2.0,1.0,0.0)
+)
+
+## one ternary node, without zero-length branch
+n2 <- parse_newick("((a:4,(b:2,c:1,e:3):1):1,d:3):1;")
+x2 <- cblv(n2)
+stopifnot(x1==x2)
+
+n <- c(
+  "((((:0.041,(:0.044,:0.32):0.62):0.2,((:0.35,:0.71):0.058,:0.21):0.54):0.064,(:0.54,:0.99):0.37):0.091);",
+  "((((:0.33,:0.93):0.076,(:0.11,:0.27):0.57):0.039,(:0.2,:0.6):0.46):0.34);",
+  "((((:0.091,:0.14):0.089,:0.1):0.037,:0.36):0.86);"
+)
+x <- cblv(parse_newick(paste(n,collapse="")))
+x1 <- cblv(parse_newick(n[1]))
+x2 <- cblv(parse_newick(n[2]))
+x3 <- cblv(parse_newick(n[3]))
+stopifnot(
+  x == rbind(x1,x2,x3)
+)
